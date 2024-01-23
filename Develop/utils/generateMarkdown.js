7e85df06ffbcc95cd1d338af3fa5e527add4a625
Efren Leal <efren@Efrens-MacBook-Pro-2.local>
2024-01-23 12:54:21 -0600

    created the license badge/link function, and created the function generating markdown dor the README file incorporating the proper sections required in the acceptance criteria

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,91 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "Apache":
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "GNU":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "BSD":
+      return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    case "None":
+      return "";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "Apache":
+      return "[Apache](https://www.apache.org/licenses/LICENSE-2.0)";
+    case "GNU":
+      return "[GNU](https://www.gnu.org/licenses/gpl-3.0.en.html)";
+    case "MIT":
+      return "[MIT](https://opensource.org/license/mit/)";
+    case "BSD":
+      return "[BSD](https://opensource.org/license/bsd-3-clause/)";
+    case "None":
+      return "";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
   return `# ${data.title}
+  ${licenseBadge}
 
+
+  ## Description 
+  ${data.description}
+
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation Instructions](#installation-instructions)
+  * [Usage Info](#usage-info)
+  * [License](#license)
+  * [Contribution Guidelines](#contribution-guidelines)
+  * [Test Instructions](#test-instructions)
+  * [Questions](#questions)
+
+
+  ## Installation Instructions
+  ${data.installation}
+
+
+  ## Usage Info
+  ${data.usage}
+
+
+  ## License
+  ${data.license} ${licenseLink}
+
+
+  ## Contribution Guidelines
+  ${data.contributions}
+
+
+  ## Test Instructions
+  ${data.testing}
+
+
+  ## Questions
+ [Email me](mailto:${data.email})
+
+ [View my other projects on GitHub!](https://www.github.com/${data.username})
 `;
 }
 
